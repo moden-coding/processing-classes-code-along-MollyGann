1dfda8063233b1fb61c3f7fd8c08c34db3bab115
MollyGann <27msokol@gannacademy.org>
2024-11-25 12:02:24 -0500

    I dont know what broke my code

--- src/App.java
+++ src/App.java
@@ -1,20 +1,46 @@
+import java.util.ArrayList;
+
 import processing.core.*;
 
-public class App extends PApplet{
-    public static void main(String[] args)  {
+public class App extends PApplet {
+    ArrayList<Bubble> bubbles;
+
+    public static void main(String[] args) {
         PApplet.main("App");
     }
 
-    public void setup(){
-        
+    public void setup() {
+        bubbles = new ArrayList<>();
     }
 
-    public void settings(){
-        
+    public void settings() {
+        size(600, 600);
     }
 
-    public void draw(){
-       
+    public void draw() {
+        background(0);
 
+        for (Bubble b : bubbles) {
+            b.display();
+            b.update();
+        }
+
+    }
+
+    public void keyPressed() {
+        if (key == ' ') {
+            int x = (int) random(500) + 20;
+            int y = (int) random(500);
+
+            Bubble bubble = new Bubble(x, y, this);
+            bubbles.add(bubble);
+
+        }
+    }
+
+    public void mousePressed() {
+        for (Bubble b : bubbles) {
+            b.checkTouch(mouseX, mouseY);
+        }
     }
 }
